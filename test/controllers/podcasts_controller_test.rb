f6d7d5d037557901a9e2edc1ff204c4fd137870b
GpoNsU <simpons@gmail.com>
2015-01-02 12:42:22 +0100

    Changed should by expect in test.

--- test/controllers/podcasts_controller_test.rb
+++ test/controllers/podcasts_controller_test.rb
@@ -5,18 +5,18 @@ class PodcastsControllerTest < ActionController::TestCase
     @podcast = podcasts(:one)
   end
 
-  test 'should get index' do
+  test 'expect get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:podcasts)
   end
 
-  test 'should get new' do
+  test 'expect get new' do
     get :new
     assert_response :success
   end
 
-  test 'should create podcast' do
+  test 'expect create podcast' do
     assert_difference('Podcast.count') do
       post :create, podcast: { audio: @podcast.audio, description: @podcast.description, thumbnail: @podcast.thumbnail,
                                title: @podcast.title }
@@ -25,23 +25,23 @@ class PodcastsControllerTest < ActionController::TestCase
     assert_redirected_to podcast_path(assigns(:podcast))
   end
 
-  test 'should show podcast' do
+  test 'expect show podcast' do
     get :show, id: @podcast
     assert_response :success
   end
 
-  test 'should get edit' do
+  test 'expect get edit' do
     get :edit, id: @podcast
     assert_response :success
   end
 
-  test 'should update podcast' do
+  test 'expect update podcast' do
     patch :update, id: @podcast, podcast: { audio: @podcast.audio, description: @podcast.description,
                                             thumbnail: @podcast.thumbnail, title: @podcast.title }
     assert_redirected_to podcast_path(assigns(:podcast))
   end
 
-  test 'should destroy podcast' do
+  test 'expect destroy podcast' do
     assert_difference('Podcast.count', -1) do
       delete :destroy, id: @podcast
     end
