3d7b3fbdefc8384dbed6d8ba62c97e0e3f1e16f5
GpoNsU <simpons@gmail.com>
2015-01-02 12:29:53 +0100

    Fixed inconsistent indentation detected.

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -62,13 +62,14 @@ class PodcastsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_podcast
-      @podcast = Podcast.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def podcast_params
-      params.require(:podcast).permit(:title, :description, :thumbnail, :audio)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_podcast
+    @podcast = Podcast.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def podcast_params
+    params.require(:podcast).permit(:title, :description, :thumbnail, :audio)
+  end
 end
