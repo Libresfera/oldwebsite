b484c6a29e8cd96b9bebd517252c56c2fd2f2978
GpoNsU <simpons@gmail.com>
2015-01-30 23:25:59 +0100

    Added restrinction to authenticate under /admin path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
 
   root 'podcasts#index'
-  devise_for :users
   resources :articles, only: [:index, :show]
   resources :podcasts, only: [:index, :show]
 
-  namespace :admin do
-    root to: 'articles#index'
-    resources :articles, except: :show
-    resources :podcasts, except: :show
+  devise_for :users
+
+  authenticate :user do
+    namespace :admin do
+      root to: 'articles#index'
+      resources :articles, except: :show
+      resources :podcasts, except: :show
+    end
   end
 end
